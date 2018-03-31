ef60c98733674fe207ce3c76e33da9197897aa97
Krzysztof Socha <ksocha@ksocha.com>
2018-03-31 17:20:53 +0200

    Rename repo

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# python-imaginary-draft
+# python-imaginary
 
-Draft version of a Python client library for [imaginary](https://github.com/h2non/imaginary), an image processing microservice.
+Python client library for [imaginary](https://github.com/h2non/imaginary), an image processing microservice.
--- src/imaginary/__init__.py
+++ src/imaginary/__init__.py
@@ -4,7 +4,7 @@ __version__ = '0.0.1'
 
 __title__ = 'imaginary'
 __description__ = 'Client library for imaginary, an image processing microservice'  # noqa: E501
-__uri__ = 'https://github.com/chaosk/python-imaginary-draft'
+__uri__ = 'https://github.com/chaosk/python-imaginary'
 __doc__ = __description__ + ' <' + __uri__ + '>'
 
 __author__ = 'Krzysztof Socha'
